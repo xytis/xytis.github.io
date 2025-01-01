f4f4f2d71e6d6ecdedc8cf90922217112f029576
Vytis Valentinavičius <xytis2000@gmail.com>
2025-01-01 17:00:23 +0200

    fix: CNAME and site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://nolife4life.org',
+	site: 'https://www.nolife4life.org',
 	integrations: [mdx(), sitemap()],
 });
