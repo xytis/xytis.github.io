45c0eb65c21d0752d695406ac8bebb650efe4c65
Vytis Valentinavičius <xytis2000@gmail.com>
2025-01-02 09:26:22 +0200

    fix: CNAME on the new domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://www.nolife4life.org',
+	site: 'https://xyt.is',
 	integrations: [mdx(), sitemap()],
 });
