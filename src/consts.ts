15f088b7f5f0a53ecf888bd4394be57bb1acfa45
Vytis Valentinavičius <vytis@nolife4life.org>
2025-10-27 14:16:18 +0200

    chore: title change

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'n4l';
+export const SITE_TITLE = 'xytis';
 export const SITE_DESCRIPTION = 'Recorded thoughts and skill issues';
